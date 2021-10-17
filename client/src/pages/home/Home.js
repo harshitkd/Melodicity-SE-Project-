869ed4250c896b40ddf579d3ad50af395764273d
glabbr-jayant <contactjaypatil@gmail.com>
2021-10-17 09:24:31 +0530

    created home page

---
+++ client/src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div className="home d-flex flex-column">
+          <div className="h1">
+            
+          </div>
+        </div>
+    )
+}
+
+export default Home
