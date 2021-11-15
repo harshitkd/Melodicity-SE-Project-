88c5f0210f1d43b03b2c5495f8f38b8cafbb360d
Avi Kumrawat <56340999+avikt18@users.noreply.github.com>
2021-11-15 19:21:36 +0530

    profile page in making

--- client/src/pages/profile/Profile.js
+++ client/src/pages/profile/Profile.js
@@ -7,14 +7,37 @@ function Profile() {
         <div className='profile-page'>
             <div className='top-background'>
                 <img src="back-icon.svg" alt='' className='back-icon' />
-                {/* <WaveAnimation
-                    iterationCount={0}
-                    color="#fff"
-                    height="35px"
-                />
-                <h1 className='profile-heading'>Your Profile</h1> */}
+                <div className="logo-container">
+                    <WaveAnimation
+                        iterationCount={0}
+                        color="#fff"
+                        height="35px"
+                    />
+                    <h1 className='profile-heading'>Your Profile</h1>
+                </div>
             </div>
-            <div className="bottom-background"></div>
+            <div className="bottom-background">
+                <div className="profile-card-glass">
+                    <div className="left-photo-section">
+                        <img src="./user-photo.svg" alt="userImage" className='user-photo' />
+                    </div>
+                    <div className="right-info-section">
+                        <h1 className='user-name'>Name</h1>
+                        <div className="artist-tag">Artist</div> {/*if user == artist*/}
+                        <div className="bottom-right-section">
+                            <div className="stats">100 Followers <div className='dot' /> 100 Listeners</div>
+                            <button className='publish-btn'> {/*if user == artist*/}
+                                <img src="./plus-icon.svg" alt="publish" className='plus-icon'/>
+                                Publish
+                            </button>
+                        </div>
+                    </div>
+                    <img src="./pencil.svg" alt="edit" className='edit-icon' />
+                </div>
+                <div className="song-section">
+
+                </div>
+            </div>
         </div>
     )
 }
