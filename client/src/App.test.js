ef510778682411148600d0b286a1bd711fe6760b
glabbr-jayant <contactjaypatil@gmail.com>
2021-11-24 22:53:13 +0530

    deployment fix

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,11 @@
-import { render } from '@testing-library/react';
-import App from './App'
+import React from 'react'
+import ReactDom from 'react-dom'
+import { Provider } from 'react-redux';
+import App from './App.js'
+import store from './redux/store.js';
 
-test('basic', () => {
-    render(<App />);
-  });
+it('renders without crashing' , () => {
+    const div = document.createElement('div');
+    ReactDom.render(<Provider store={store}><App /></Provider>, div);
+    ReactDom.unmountComponentAtNode(div);
+})
--- client/src/components/userCard/UserCard.js
+++ client/src/components/userCard/UserCard.js
@@ -8,7 +8,7 @@ function UserCard({user}) {
     }
     return (
         <div className='song-card' onClick={handleClick}>
-            <img src={user.cover ? user.cover : "/user-photo.svg"} alt="song-cover" className='song-cover' />
+            <img src={user.cover ? user.cover : "/user-photo.svg"} alt="song-cover" className='song-cover' style={{minHeight : "140px", minWidth:"140px"}}/>
             <div className="card-info">
                 <h1 className='song-name'>{ user.name}</h1> 
             </div>
